878db9cb7dde7ef60a3ae8543bcd92c08bd7b65d
Asti Ananta <astiananta914@gmail.com>
2025-03-19 16:43:26 -0600

    Add files via upload

---
+++ Hasil daftar belanja.py
@@ -0,0 +1,54 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\LENOVO\OneDrive\Documents\Daftar belanja.py ========
+
+Menu: 
+1. Tambah Item: 
+2. Lihat Daftar Belanja: 
+3. Hapus Item: 
+4. Keluar
+Pilih menu (1-4): 1
+Masukkan nama item: mie ayam dan matcha
+"mie ayam dan matcha"telah ditambahkan ke daftar belanja.
+
+Menu: 
+1. Tambah Item: 
+2. Lihat Daftar Belanja: 
+3. Hapus Item: 
+4. Keluar
+Pilih menu (1-4): 1
+Masukkan nama item: bakso dan es teh
+"bakso dan es teh"telah ditambahkan ke daftar belanja.
+
+Menu: 
+1. Tambah Item: 
+2. Lihat Daftar Belanja: 
+3. Hapus Item: 
+4. Keluar
+Pilih menu (1-4): 2
+
+Daftar Belanja.
+1. mie ayam dan matcha
+2. bakso dan es teh
+
+Menu: 
+1. Tambah Item: 
+2. Lihat Daftar Belanja: 
+3. Hapus Item: 
+4. Keluar
+Pilih menu (1-4): 3
+
+Daftar Belanja.
+1. mie ayam dan matcha
+2. bakso dan es teh
+Masukkan nomor item yang ingin di hapus: 1
+"bakso dan es teh" telah dihapus dari dafftar belanja.
+
+Menu: 
+1. Tambah Item: 
+2. Lihat Daftar Belanja: 
+3. Hapus Item: 
+4. Keluar
+Pilih menu (1-4): 4
+Terimakasih! Program selesai.
